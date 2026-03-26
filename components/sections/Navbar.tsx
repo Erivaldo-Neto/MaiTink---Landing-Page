d3b4e27138225232f5d8e1b84474ee288164d619
Neto <erivaldoneto2007@gmail.com>
2026-03-26 20:15:18 -0300

    fix: remove unused React import in sections/Navbar.tsx

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { SITE_DATA } from "@/lib/constants";
